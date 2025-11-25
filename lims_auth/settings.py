d46566643c3b6c88d3568e08bcf66ed772c14b7f
Iyanu Arowosola <iarowosola@yahoo.com>
2025-11-25 17:54:28 +0100

    testing-on

--- lims_auth/settings.py
+++ lims_auth/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = False  # overridden in dev.py
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost', '127.0.0.1', "firstjp-lims-web.onrender.com"
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
