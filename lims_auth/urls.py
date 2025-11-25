5451515b4930ab9578658583881d4a28711d7f21
Iyanu Arowosola <iarowosola@yahoo.com>
2025-11-25 21:52:18 +0100

    fix bug

--- lims_auth/urls.py
+++ lims_auth/urls.py
@@ -14,36 +14,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-
 from django.contrib import admin
 from django.urls import path, include
-# from apps.core import views
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
-
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('apps.core.urls')),
-   
     path("account/", include("apps.accounts.urls", namespace="account")),
-
     path("tenants/", include("apps.tenants.urls")),
-    
     path("laboratory/", include("apps.labs.urls", namespace="labs")),
-
-    path("lms/", include("apps.lms.urls", namespace="lms")), # LMS app URLs
+    path("lms/", include("apps.lms.urls", namespace="lms")),
 ]
 
-# Serve static and media files during development
+# Static & media files (dev only)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# Debug Toobar 
-urlpatterns += debug_toolbar_urls()
-urlpatterns += [
-    path("__reload__/", include("django_browser_reload.urls")),
-]
+# Debug Toolbar (development only)
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
