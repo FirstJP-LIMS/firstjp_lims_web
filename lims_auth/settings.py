6d09fbe58d032126bd9fecc402b81158909f9dec
Iyanu Arowosola <iarowosola@yahoo.com>
2026-01-13 18:51:49 +0100

    Staticfiles

--- lims_auth/settings.py
+++ lims_auth/settings.py
@@ -229,8 +229,13 @@ if ENVIRONMENT == "production":
     # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
     # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+
     STATIC_URL = "/static/"
     STATIC_ROOT = BASE_DIR / "staticfiles"
+
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
 
     
     # Redis server - RENDER
