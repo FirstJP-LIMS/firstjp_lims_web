75ed27f335939f8bc6adc0813bece1f2227bd60b
Iyanu Arowosola <iarowosola@yahoo.com>
2026-01-13 17:02:36 +0100

    https excluded

--- lims_auth/settings.py
+++ lims_auth/settings.py
@@ -252,9 +252,9 @@ if ENVIRONMENT == "production":
 
  
     # Security settings for production
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
+    # SECURE_SSL_REDIRECT = True
+    # SESSION_COOKIE_SECURE = True
+    # CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
     X_FRAME_OPTIONS = 'DENY'
